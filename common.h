dcc801770e2ee21cdd9c0cf1d43aac39c2b823ce
trackersite <ckorpak@gmail.com>
2019-05-11 19:48:32 +0200

    Update common.h

--- common.h
+++ common.h
@@ -3,15 +3,20 @@
 
 #define PORT_TCP 8080
 #define PORT_UDP 8888
-#define TAILLE_BUFFER 200
+#define TAILLE_BUFFER 1024
 #define TRUE   1
 #define FALSE  0
 #define MULTICAST_GROUP "226.1.2.3"
 #define MULTICAST_IP "127.0.1.1"
 
-struct clients_connectes {
-  char *pseudo;
-  struct sockaddr_in infos;
+struct info_client {
+  char pseudo[24];
+  char adresse[24];
+};
+struct liste_client {
+  char pseudo[24];
+  char adressetcp[24];
+  char identifiant;
 };
 
 int creerSocketTCP();
